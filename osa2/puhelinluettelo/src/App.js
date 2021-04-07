7974b02d6db546a84a5b3a83b00d0eedf6b1ea18
rekekki <r2101k@pm.me>
2021-04-07 07:11:09 +0300

    imported components, declared states and eventhandlers

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,26 @@
 import React, { useState } from 'react'
+import Persons from '.components/Persons'
+import NewPerson from '.components/NewPerson'
+import FilterPerson from '.components/FilterPerson'
 
 const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
